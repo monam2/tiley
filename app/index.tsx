51c051ccc58bcd2addb8a8606392abe0609bc132
monam2 <kangcw0107@gmail.com>
2025-07-27 22:49:10 +0900

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+
+import { ThemedText } from "@/components/ThemedText";
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <ThemedText>Hello World</ThemedText>
+    </View>
+  );
+}
