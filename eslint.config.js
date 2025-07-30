d70bd39952b1b99deaa457609a378c1a5ba9a71d
monam2 <kangcw0107@gmail.com>
2025-07-30 20:44:01 +0900

    chore: 주요 패키지 설치

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,20 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
 
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ['dist/*'],
+    ignores: ["dist/*"],
+  },
+  // 테스트 파일용 설정 추가
+  {
+    files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+    plugins: {
+      "testing-library": require("eslint-plugin-testing-library"),
+    },
+    rules: {
+      ...require("eslint-plugin-testing-library").configs.react.rules,
+    },
   },
 ]);
